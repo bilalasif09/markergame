3f049d7ab7014c1a5432c14d014e2a1b9ed32433
bilal.asif <bilal.asif@khaleef.com>
2018-10-11 19:13:02 +0500

    gameplay client

--- src/App.js
+++ src/App.js
@@ -6,44 +6,86 @@ class Board extends Component {
     super(props);
     this.state = {
       boxes: Array(64).fill({value: null, disabledState: false}),
-      currentPlayer: null,
-      currentPlayerPosition: null,
-      otherPlayerPosition: null
+      currentPlayer: 'red',
+      otherPlayer: 'green',
+      currentPlayerPosition: 3,
+      otherPlayerPosition: 60
     };
   };
   componentWillMount() {
-    this.setState({currentPlayer: 'red', currentPlayerPosition: 3, otherPlayerPosition: 60}, () => {
-      this.setButtonStates();
-    });
+    this.setButtonStates();
   };
   getPlayerMoves() {
     const currentPosition = this.state.currentPlayerPosition;
-    let moves = [currentPosition+1,currentPosition-1,currentPosition+8,currentPosition-8,currentPosition+7,
-      currentPosition-7,currentPosition+9,currentPosition-9];
+    let moves = [];
+    if (currentPosition % 8 !== 0 && currentPosition % 8 !== 7) { // if not on corners
+      moves = [currentPosition+1,currentPosition-1,currentPosition+8,currentPosition-8,currentPosition+7,
+        currentPosition-7,currentPosition+9,currentPosition-9];
+    } else if (currentPosition % 8 === 0) { // if left cornor
+      moves = [currentPosition+1,currentPosition+8,currentPosition-8,currentPosition+9,currentPosition-7];
+    } else if (currentPosition % 8 === 7) { // if right cornor
+      moves = [currentPosition-1,currentPosition+8,currentPosition-8,currentPosition-9,currentPosition+7];
+    };
     return moves.filter(move => move>=0 && move<=63);
   };
   setButtonStates() {
     let boxes = this.state.boxes.slice();
+    boxes[this.state.currentPlayerPosition] = { value: this.state.currentPlayer, disabledState: true };
+    boxes[this.state.otherPlayerPosition] = { value: this.state.otherPlayer, disabledState: true };
     const playerMoves = this.getPlayerMoves();
-    console.log("Player moves", playerMoves);
+    console.log("Player moves", playerMoves, "boxes before", boxes);
     for (let i=0; i < boxes.length; i++) {
       if (!boxes[i].disabledState && playerMoves.indexOf(i) === -1) {
-        boxes[i] = { value: null, disabledState: true };
+        boxes[i] = { value: boxes[i].value, disabledState: true };
+      } else if (!boxes[i].value && playerMoves.indexOf(i) !== -1) {
+        boxes[i] = { value: boxes[i].value, disabledState: false };
       };
     };
+    console.log("Boxes after", boxes);
     this.setState({boxes: boxes});
   };
   handleClick(boxNumber) {
     let boxes = this.state.boxes.slice();
     boxes[boxNumber] = { value: this.state.currentPlayer, disabledState: true };
-    this.state.currentPlayer === 'red'
-     ? this.setState({currentPlayer: 'green'})
-     : this.setState({currentPlayer: 'red'});
-    this.setState({boxes: boxes}); 
+    let tempPlayer = this.state.currentPlayer;
+    const promise1 = new Promise( (resolve) => {
+      this.setState({currentPlayer: this.state.otherPlayer}, () => {
+        this.setState({otherPlayer: tempPlayer}, () => {
+          console.log("Promise 1", this.state);
+          resolve();
+        });
+      });
+    }); 
+    const promise2 = new Promise( (resolve) => {
+      this.setState({currentPlayerPosition: this.state.otherPlayerPosition}, () => {
+        this.setState({otherPlayerPosition: boxNumber}, () => {
+          console.log("Promise 2", this.state);
+          resolve();
+        });
+      });
+    });
+    const promise3 = new Promise( (resolve) => {
+      this.setState({boxes: boxes}, () => {
+        console.log("Promise 3", this.state);
+        resolve();
+      });
+    });
+    Promise.all([promise1, promise2, promise3])
+    .then( values => {
+      console.log("Promise all", values, this.state);
+      this.setButtonStates();
+    })
+    .catch( err => {
+      console.log("Error resolving promises", err);
+    });
+     
   };
   renderBoxes(boxNumber) {
     return(<Box key={boxNumber} value={this.state.boxes[boxNumber]}
-     onClick={() => this.handleClick(boxNumber)} />);  
+      currentPlayerPosition={this.state.currentPlayerPosition}
+      otherPlayerPosition={this.state.otherPlayerPosition}
+      index={boxNumber}
+      onClick={() => this.handleClick(boxNumber)} />);  
   };
   renderBoard() {
     let board = [];
@@ -76,7 +118,9 @@ class Box extends Component {
     return (
       <button disabled={this.props.value.disabledState} className={[this.props.value.value, "box"].join(' ')}
        onClick={() => this.props.onClick()}>
-        {this.props.value.value}
+        {this.props.currentPlayerPosition === this.props.index || this.props.otherPlayerPosition === this.props.index 
+          ? this.props.value.value.charAt(0)
+          : ''}
       </button>
     );
   }
